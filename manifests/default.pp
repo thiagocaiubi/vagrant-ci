6a79359e0007f922d573fa1514aea625b75f3725
Thiago Caiubi <thiago.caiubi@gmail.com>
2012-08-15 22:39:41 -0300

    Ensures apt-get update before installing packges. Includes modules stdlib, wget, java and maven.

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,3 +1,16 @@
-import "git"
+import 'git'
+import 'maven'
+
+exec { 'apt-get update':
+	command => '/usr/bin/apt-get update'
+}
 
 class { 'git::client': }
+
+class { 'java':
+	distribution => 'jdk',
+	version      => 'latest',
+} ->
+class { 'maven::maven' :
+	version => '3.0.4',
+}
