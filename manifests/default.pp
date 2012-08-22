be5f9af42bf07fa1bd7393955a0da204c46d8c8b
Thiago Caiubi <thiago.caiubi@gmail.com>
2012-08-22 20:39:27 -0300

    Installs jenkins

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,17 +1,21 @@
-exec { 'apt-get update':
-	command => '/usr/bin/apt-get update'
+exec { 'apt_update':
+	command     => "/usr/bin/apt-get update --fix-missing",
+	logoutput   => 'on_failure',
 }
 
 class { 'git::client': }
 
-class { 'unzip': } ->
+class { 'unzip': } 
+
 class { 'java':
 	distribution => 'jdk',
 	version      => 'latest',
+	require => Exec['apt_update'],
 } ->
 class { 'maven::maven' : 
 	version => '3.0.4',
 } ->
+class { 'jenkins::package': } ->
 class { 'sonar' :
 	version      => '3.2',
 	arch         => 'linux-x86-64',
